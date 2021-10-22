6e9218eac02b38a6a79ba359ce1e82287804e9a6
Amiolemen David <Amidavid01@gmail.com>
2021-10-22 16:15:35 +0100

    tweaked the JS to get the TotalBilll better

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 // let totalBill= document.getElementById('totalBill');
 
 let button=document.getElementById('btn');
+let input=document.getElementById("input-area");
 let tipPercentage= document.getElementsByClassName("tip")
 let totalTip= document.getElementById("total-tip")
 let numberOfPeople= document.getElementById('numberOfPeople')
@@ -10,13 +11,10 @@ let tipPerPerson= document.getElementsByClassName("amount-per-person")
 
 
 
-function getInput(){
-    inputArea=document.getElementById("input-area").value;
-}
 
 
 button.addEventListener('click',()=>{
-    getInput();
+    const inputArea=input.value
     let tipPercentage=5
     let totalFee= inputArea
     let tipPerPerson= (tipPercentage/100)* totalFee
